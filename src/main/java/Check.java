cc8232f04ffb0f090b37f3774e4cc4f75f58fd36
QUEAHREN <xsyooo@icloud.com>
2021-11-21 01:35:22 +0800

    Creat feeds class

--- src/main/java/Check.java
+++ src/main/java/Check.java
@@ -5,28 +5,76 @@ import com.alibaba.fastjson.JSONObject;
 import java.awt.*;
 import java.io.*;
 
+
 public class Check {
 
     public static void main(String[] args) throws IOException {
 
 
 
-        InputStream is = new FileInputStream("C:/Users/xsyoo/Desktop/Demo/Demo0.js");
+        InputStream is = new FileInputStream("C:/Users/xsyoo/Desktop/Demo/DATA0.json");
         String line; // 用来保存每行读取的内容
         StringBuffer buffer = new StringBuffer();
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         line = reader.readLine(); // 读取第一行
+
+
+        int flag = 0;
         while (line != null) { // 如果 line 为空说明读完了
-            buffer.append(line); // 将读到的内容添加到 buffer 中
-            buffer.append("\n"); // 添加换行符
+            if (line.equals("\t\"data\":")) {
+                line = reader.readLine(); // 读取下一行
+                buffer.append(line); // 将读到的内容添加到 buffer 中
+                buffer.append("\n"); // 添加换行符
+                break;
+            }
             line = reader.readLine(); // 读取下一行
+
         }
         reader.close();
         is.close();
 
-        System.out.println(buffer);
+        String data_s = buffer.toString();
 
-//        JSONObject jsonobject = JSON.parseObject();
+        int beginIndex = 0;
+//        for (int i = 0; i < data_s.length(); i ++){
+//            if (data_s.charAt(i) == '[') {
+//                beginIndex = i;
+//                break;
+//            }
+//        }
+        String JsonData = data_s.substring(beginIndex, data_s.length()-2);
+
+        System.out.println(JsonData);
+        JSONObject jsonObject = JSON.parseObject(JsonData);
+
+        JSONObject main = jsonObject.getJSONObject ( "main" ) ;
+        System.out.println(main);
+        JSONArray data = jsonObject.getJSONArray("data");
+        System.out.println(data);
+
+        for (int i = 0; i < 2; i++) {
+
+            JSONObject datai = data.getJSONObject(i);
+
+            String feedstime = datai.getString("feedstime");
+            String html = datai.getString("html");
+            String logimg = datai.getString("logimg");
+            String nickname = datai.getString("nickname");
+            String opuid = datai.getString("opuid");
+            String uin = datai.getString("uin");
+
+            Feeds feeds = new Feeds();
+            feeds.setFeedstime(feedstime);
+            feeds.setHtml(html);
+            feeds.setLogimg(logimg);
+            feeds.setNickname(nickname);
+            feeds.setOpuin(opuid);
+            feeds.setUin(uin);
+
+            System.out.println(feeds.toString());
+
+        }
+
 
 
     }
---
+++ src/main/java/Feeds.java
@@ -0,0 +1,61 @@
+public class Feeds {
+
+    private String opuin;
+    private String nickname;
+    private String uin;
+    private String logimg;
+    private String feedstime;
+    private String html;
+
+    public String getOpuin() {
+        return opuin;
+    }
+    public String getUin() {
+        return uin;
+    }
+    public String getFeedstime() {
+        return feedstime;
+    }
+    public String getHtml() {
+        return html;
+    }
+    public String getLogimg() {
+        return logimg;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setFeedstime(String feedstime) {
+        this.feedstime = feedstime;
+    }
+    public void setHtml(String html) {
+        this.html = html;
+    }
+    public void setLogimg(String logimg) {
+        this.logimg = logimg;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public void setOpuin(String opuin) {
+        this.opuin = opuin;
+    }
+    public void setUin(String uin) {
+        this.uin = uin;
+    }
+
+
+    @Override
+    public String toString(){
+
+        String str = "昵称:" + nickname + "\n" +
+                "log链接:" + logimg + "\n" +
+                "时间:" + feedstime + "\n" +
+                "opuin:" + opuin + "\n" +
+                "uin:" + uin + "\n" +
+                "html:" + html + "\n";
+        return str;
+
+    }
+}
